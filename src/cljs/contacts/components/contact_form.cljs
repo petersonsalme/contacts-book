0a4acc8ac49b4539e6e5f40a76977421ae5a7bb5
Peterson Salme <petersonsalme@gmail.com>
2022-11-18 20:27:00 -0300

    Creating and populating contact list and form

---
+++ src/cljs/contacts/components/contact_form.cljs
@@ -0,0 +1,56 @@
+(ns contacts.components.contact-form
+  (:require [helix.core :refer [defnc $ <>]]
+            [helix.dom :as d]
+            [helix.hooks :as hooks]
+            [clojure.string :as s]))
+
+(def contact-form-fields
+  ["first_name" "last_name" "email"])
+
+(defn make-label-str [s]
+  (str (-> s
+           (s/replace #"_" " ")
+           (s/capitalize))
+       ":"))
+
+(defnc contact-display-item [{:keys [label value]}]
+  (d/p (d/strong label) value))
+
+(defnc contact-display [{:keys [contact]}]
+  (<>
+   (map-indexed 
+    (fn [i v] ($ contact-display-item {:key i
+                                       :label (make-label-str v)
+                                       :value (get contact (keyword v))})) 
+    contact-form-fields)))
+
+(defnc contact-edit-item [{:keys [label value on-change]}]
+  (d/div
+   (d/label {:for label} (make-label-str label))
+   (d/input {:id label 
+             :value value
+             :on-change on-change})))
+
+(defnc contact-edit [{:keys [contact]}]
+  (let [[state set-state] (hooks/use-state contact)]
+    (d/form
+     (map-indexed
+      (fn [i v] ($ contact-edit-item {:key i
+                                      :label v
+                                      :value (get state (keyword v)) 
+                                      :on-change #(set-state 
+                                                   (assoc state 
+                                                          (keyword v)
+                                                          (.. %
+                                                              -target
+                                                              -value)))}))
+      contact-form-fields))))
+
+(defnc contact-form [{:keys [contact]}]
+  (let [[edit set-edit] (hooks/use-state false)]
+    (d/div
+     (d/h1 "Contact form")
+     (d/button {:on-click #(set-edit (not edit))} "toogle")
+     (if edit 
+       ($ contact-edit {:contact contact})
+       ($ contact-display {:contact contact})))))
--- src/cljs/contacts/core.cljs
+++ src/cljs/contacts/core.cljs
@@ -1,24 +1,24 @@
 (ns contacts.core
   (:require [ajax.core :refer [GET]]
-            [helix.core :refer [defnc $]]
+            [helix.core :refer [defnc $ <>]]
+            [helix.hooks :as hooks]
             [helix.dom :as d]
-            ["react-dom/client" :as rdom]))
-
-(defnc nav []
-  (d/nav {:class '[py-4 shadow]}
-   (d/div {:class '[container]}
-    (d/h2 {:class '[text-xl]} "Contact book"))))
+            ["react-dom/client" :as rdom]
+            [contacts.components.nav :refer [nav]]
+            [contacts.components.contact-list :refer [contact-list]]
+            [contacts.components.contact-form :refer [contact-form]]))
 
 (defnc app []
-  (d/div 
-   ($ nav)))
+  (let [[state set-state] (hooks/use-state nil)]
+    (hooks/use-effect
+     :once
+     (GET "http://localhost:4000/api/contacts" {:handler (fn [resp] (set-state resp))}))
+    (<>
+     ($ nav)
+     (d/div {:class '[container pt-4]} 
+            ($ contact-list {:contacts state})
+            ($ contact-form {:contact (first state)})))))
 
-(defn ^:export init []
+(defn ^:export ^:dev/after-load init []
   (let [root (rdom/createRoot (js/document.getElementById "app"))]
     (.render root ($ app))))
-
-(comment
-  (GET "http://localhost:4000/api/contacts"
-    {:handler (fn [resp]
-                (.log js/console resp))})
-  )
